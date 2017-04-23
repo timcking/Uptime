e472bc7a1674ea5193b5cf2acbaaf1eecd829bfd
timcking <timcking@gmail.com>
2017-04-22 18:40:07 -0700

    Added Action Menu Bar

--- app/src/main/java/com/timothyking/uptime/MainActivity.java
+++ app/src/main/java/com/timothyking/uptime/MainActivity.java
@@ -6,21 +6,44 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private String alertDay;
-
     public void onRefreshClick (View view) {
         setUptime();
     }
-
     public void onExitClick (View view) {
         this.finishAffinity();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 
     public void setUptime() {
